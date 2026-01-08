50a5caef38e2c6175bbe9ddef12d7ff710f5d321
Fenitra12 <fenitra00@gmail.com>
2026-01-08 20:39:14 +0300

    test

--- src/main/java/com/aerienne/gestion/controller/SimpleController.java
+++ src/main/java/com/aerienne/gestion/controller/SimpleController.java
@@ -11,16 +11,28 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.aerienne.gestion.entity.SimpleEntity;
 import com.aerienne.gestion.repository.SimpleEntityRepository;
+import com.aerienne.gestion.service.SimpleService;
 
 @RestController
 @RequestMapping("/api/simple")
+
 public class SimpleController {
     @Autowired
     private SimpleEntityRepository simpleEntityRepository;
 
+    @Autowired
+    private SimpleService simpleService;
+
+
     @GetMapping
     public List<SimpleEntity> getAll() {
         return simpleEntityRepository.findAll();
+    }
+
+    // Endpoint pour récupérer le pourcentage depuis le service
+    @GetMapping("/percentage-a")
+    public double getPercentageNameStartsWithA() {
+        return simpleService.percentageNameStartsWithA();
     }
 
     @PostMapping
